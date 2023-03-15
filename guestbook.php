67e276c5875d12836c65fac7cf370226f699f081
Lavrov Nikita <m.o.lavrov@student.khai.edu>
2023-03-15 11:08:31 +0200

    Writed guestbook.php

--- guestbook.php
+++ guestbook.php
@@ -2,6 +2,13 @@
 // TODO 1: PREPARING ENVIRONMENT: 1) session 2) functions
 session_start();
 
+if (!empty($_POST)){
+    $jsonString = json_encode($_POST);
+    $fileStream = fopen ('comments.csv','a');
+    fwrite($fileStream , $jsonString ."\n");
+    fclose($fileStream);
+}
+
 // TODO 2: ROUTING
 
 // TODO 3: CODE by REQUEST METHODS (ACTIONS) GET, POST, etc. (handle data from request): 1) validate 2) working with data source 3) transforming data
@@ -34,6 +41,24 @@ session_start();
                 <div class="col-sm-6">
 
                  <!-- TODO: create guestBook html form   -->
+                    <form method="post">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input class="form-control" type="email" name="email"/>
+                        </div>
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input class="form-control" type="text" name="name"/>
+                        </div>
+                        <div class="form-group">
+                            <label>Comments</label>
+                            <input class="form-control" type="text" name="comment"/>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" name="form"/>
+                        </div>
+                    </form>
 
                 </div>
             </div>
@@ -52,6 +77,20 @@ session_start();
                 <div class="col-sm-6">
 
                     <!-- TODO: render guestBook comments   -->
+                    <?php
+                    if ( file_exists ('comments.csv')){
+                        $fileStream = fopen('comments.csv', "r");
+                        while(!feof($fileStream)){
+                            $jsonString = fgets ($fileStream);
+                            $array = json_decode ($jsonString, true);
+                            if (empty ($array)) break ;
+                            echo $array ['email'] . '<br>';
+                            echo '<b>' . $array ['name'] . '<br>' . '</b>';
+                            echo $array ['comment'] . '<br>';
+                        }
+                        fclose ($fileStream );
+                    }
+                    ?>
 
                 </div>
             </div>
